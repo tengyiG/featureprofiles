eab732a7702b6874639716cea0cbd5ae7b093204
Tengyi Wang <tengyi@google.com>
2025-06-17 17:38:42 -0700

    Built up basic framework for testing.

--- feature/gribi/otg_tests/mpls_in_udp_scale/util.go
+++ feature/gribi/otg_tests/mpls_in_udp_scale/util.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package mpls_in_udp_scale_test
+package mpls_in_udp_scale
 
 import (
 	"context"
